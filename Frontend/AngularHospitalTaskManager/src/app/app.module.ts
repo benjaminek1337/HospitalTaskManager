dd527e945710b1ad93f8097243cb459f2371731a
Benjamin Ek <benjaminek@live.se>
2020-04-08 09:19:10 +0200

    Added navigation

--- Frontend/AngularHospitalTaskManager/src/app/app.module.ts
+++ Frontend/AngularHospitalTaskManager/src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DevExtremeModule } from "devextreme-angular";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {MatSidenavModule} from '@angular/material/sidenav'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { fromEventPattern } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DevExtremeModule
+    DevExtremeModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
